840d9f509ea7a63d4aafc1f13958893b67163519
yoshiioko <acardoza@pdx.edu>
2022-12-18 15:52:00 -0800

    implement createNewMint() function to generate Token Mint account.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,43 @@
-import { initializeKeypair } from "./initializeKeypair"
-import * as web3 from "@solana/web3.js"
+import { initializeKeypair } from "./initializeKeypair";
+import * as web3 from "@solana/web3.js";
+import * as token from "@solana/spl-token";
+
+async function createNewMint(
+  connection: web3.Connection,
+  payer: web3.Keypair,
+  mintAuthority: web3.PublicKey,
+  freezeAuthority: web3.PublicKey,
+  decimals: number
+): Promise<web3.PublicKey> {
+  const tokenMint = await token.createMint(
+    connection,
+    payer,
+    mintAuthority,
+    freezeAuthority,
+    decimals
+  );
+
+  console.log(`The token mint account address is ${tokenMint}`);
+  console.log(
+    `Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`
+  );
+
+  return tokenMint;
+}
 
 async function main() {
-  const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
-  const user = await initializeKeypair(connection)
+  const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+  const user = await initializeKeypair(connection);
 
-  console.log("PublicKey:", user.publicKey.toBase58())
+  console.log("PublicKey:", user.publicKey.toBase58());
 }
 
 main()
   .then(() => {
-    console.log("Finished successfully")
-    process.exit(0)
+    console.log("Finished successfully");
+    process.exit(0);
   })
   .catch((error) => {
-    console.log(error)
-    process.exit(1)
-  })
+    console.log(error);
+    process.exit(1);
+  });
