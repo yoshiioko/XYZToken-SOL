116109985b307480667e6854e817d37e62e83103
yoshiioko <acardoza@pdx.edu>
2022-12-18 18:35:03 -0800

    implement function to mint tokens. add calls to function main() to follow all steps to mint SPL token

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { initializeKeypair } from "./initializeKeypair";
 import * as web3 from "@solana/web3.js";
 import * as token from "@solana/spl-token";
+import { mintTo } from "@solana/spl-token";
+
+// Token Mint Address: EMdrVqe5Pb5UtbXitNGeGr5jByAF41fQfYHj7Nc9XaKq
 
 async function createNewMint(
   connection: web3.Connection,
@@ -45,11 +48,52 @@ async function createTokenAccount(
   return tokenAccount;
 }
 
+async function mintTokens(
+  connection: web3.Connection,
+  payer: web3.Keypair,
+  mint: web3.PublicKey,
+  destination: web3.PublicKey,
+  authority: web3.Keypair,
+  amount: number
+) {
+  const mintInfo = await token.getMint(connection, mint);
+
+  const transactionSignature = await token.mintTo(
+    connection,
+    payer,
+    mint,
+    destination,
+    authority,
+    amount * 10 ** mintInfo.decimals
+  );
+
+  console.log(
+    `Mint Token Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  );
+}
+
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
   const user = await initializeKeypair(connection);
 
   console.log("PublicKey:", user.publicKey.toBase58());
+
+  const mint = await createNewMint(
+    connection,
+    user,
+    user.publicKey,
+    user.publicKey,
+    2
+  );
+
+  const tokenAccount = await createTokenAccount(
+    connection,
+    user,
+    mint,
+    user.publicKey
+  );
+
+  await mintTokens(connection, user, mint, tokenAccount.address, user, 100);
 }
 
 main()
